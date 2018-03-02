10136e4e0b267432992a6467e46b65bc2f7ccace
Rhys Kyte <rskyte@gmail.com>
2018-03-02 20:36:19 +0000

    all e2e test (apart from routing) added

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -21,8 +21,69 @@ module.exports = {
       .click('ul.todo-list li:first-child')
       .doubleClick()
       .setValue('input[class=edit]', ['edit', browser.Keys.ENTER])
-      .assert.containsText('ul.todo-list li:first-child',
-        'edit')
+      .assert.containsText('ul.todo-list li:first-child', 'edit')
+      .end();
+  },
+
+  'Deleting Todos': (browser) => {
+    const devServer = browser.globals.devServerURL
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .setValue('input[class=new-todo]', ['test', browser.Keys.ENTER])
+      .waitForElementVisible('ul.todo-list li:first-child > div > button.destroy', 5000)
+      .click('ul.todo-list li:first-child > div > button.destroy')
+      .pause(500)
+      .assert.elementNotPresent('ul.todo-list li:first-child')
+      .end();
+  },
+
+  'Checking off completed Todos': (browser) => {
+    const devServer = browser.globals.devServerURL
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .setValue('input[class=new-todo]', ['test', browser.Keys.ENTER])
+      .pause(500)
+      .click('input[type=checkbox][class=toggle]')
+      .pause(500)
+      .assert.elementPresent('.completed')
+      .end();
+  },
+
+  'Clearing completed Todos': (browser) => {
+    const devServer = browser.globals.devServerURL
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .setValue('input[class=new-todo]', ['test', browser.Keys.ENTER])
+      .pause(500)
+      .click('input[type=checkbox][class=toggle]')
+      .pause(500)
+      .click('.clear-completed')
+      .pause(500)
+      .assert.elementNotPresent('ul.todo-list li:first-child')
+      .end();
+  },
+
+  'Displays the correct message for remaining Todos': (browser) => {
+    const devServer = browser.globals.devServerURL
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .setValue('input[class=new-todo]', ['test', browser.Keys.ENTER])
+      .pause(500)
+      .assert.containsText('.todo-count', '1 item left')
+      .setValue('input[class=new-todo]', ['test2', browser.Keys.ENTER])
+      .pause(500)
+      .assert.containsText('.todo-count', '2 items left')
+      .click('ul.todo-list li:first-child > div > input[type=checkbox][class=toggle]')
+      .pause(500)
+      .assert.containsText('.todo-count', '1 item left')
+      .click('ul.todo-list li:last-child > div > input[type=checkbox][class=toggle]')
+      .pause(500)
+      .assert.containsText('.todo-count', '0 items left')
       .end();
   }
+
 };
