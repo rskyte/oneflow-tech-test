eb2143db5527b522f61826b11917c86c5f300342
Rhys Kyte <rskyte@gmail.com>
2018-03-02 00:42:03 +0000

    unit tests written

--- test/unit/specs/unit.spec.js
+++ test/unit/specs/unit.spec.js
@@ -3,12 +3,7 @@ import app from '@/App';
 
 describe('app.vue', () => {
 
-  // beforeEach(() => {
-  //   const Constructor = Vue.extend(app)
-  //   const subject = new Constructor().$mount()
-  // });
-
-  it("adds todo's to list", function() {
+  it("adds todo's to list", () => {
     const Constructor = Vue.extend(app)
     const subject = new Constructor().$mount()
     subject.newTodo = 'test'
@@ -16,5 +11,51 @@ describe('app.vue', () => {
     testAddTodo()
     expect(subject.todos[0].title).to.equal('test')
   });
+
+  it("removes todo's from list", function() {
+    const Constructor = Vue.extend(app)
+    const subject = new Constructor().$mount()
+    subject.newTodo = 'test'
+    let testAddTodo = subject.$options.methods.addTodo.bind(subject)
+    testAddTodo()
+    let testRemoveTodo = subject.$options.methods.removeTodo.bind(subject)
+    testRemoveTodo(subject.todos[0])
+    expect(subject.todos).to.be.empty
+  });
+
+  it("edits todo's in list", function() {
+    const Constructor = Vue.extend(app)
+    const subject = new Constructor().$mount()
+    subject.newTodo = 'test'
+    let testAddTodo = subject.$options.methods.addTodo.bind(subject)
+    testAddTodo()
+    let testEditTodo = subject.$options.methods.editTodo.bind(subject)
+    testEditTodo(subject.todos[0])
+    let testDoneEdit = subject.$options.methods.doneEdit.bind(subject)
+    subject.todos[0].title = 'edit'
+    testDoneEdit(subject.todos[0])
+    expect(subject.todos[0].title).to.equal('edit')
+  });
+
+  it("can cancel edit if needed", function() {
+    const Constructor = Vue.extend(app)
+    const subject = new Constructor().$mount()
+    subject.newTodo = 'test'
+    let testAddTodo = subject.$options.methods.addTodo.bind(subject)
+    testAddTodo()
+    let testEditTodo = subject.$options.methods.editTodo.bind(subject)
+    testEditTodo(subject.todos[0])
+    let testCancelEdit = subject.$options.methods.cancelEdit.bind(subject)
+    testCancelEdit(subject.todos[0])
+    expect(subject.todos[0].title).to.equal('test')
+    expect(subject.editedTodo).to.equal(null)
+  });
+
+  it("updates storage whenever changes are made to todo's", function() {
+    const Constructor = Vue.extend(app)
+    const subject = new Constructor().$mount()
+    console.log(app)
+    //sinon.spy(todoStorage)
+  })
 
 });
